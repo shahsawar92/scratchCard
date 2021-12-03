d1db0c4322c7457e7158b9c206fe9a04b2fe8123
shahsawar92 <shahsawar92@gmail.com>
2021-12-03 14:36:39 +0500

    added logo

--- src/Components/header.js
+++ src/Components/header.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import logo from '../logo.png';
 
 export default function Header() {
     return (
-        <div className="w-screen h-1/6 bg-yellow-300">
-            
+        <div className="w-screen h-24 bg-yellow-300">
+            <span  >
+                <img className="h-20 mt-4 ml-12 " src={logo}></img>
+            </span>
         </div>
     )
 }
